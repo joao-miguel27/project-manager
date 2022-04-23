b17d8a5de2a299b59075d99a3e88cf2b69844544
João Miguel <joaomiguelneves2007@gmail.com>
2022-04-23 13:21:27 -0300

    .

--- scr/gui/mainframe.cpp
+++ scr/gui/mainframe.cpp
@@ -1,41 +1,35 @@
-#include"mainframe.h"
-#include"iostream"
-#include"write_project.h"
-#include"read_project.h"
+#include "mainframe.h"
+#include "iostream"
+#include "read_project.h"
+#include "write_project.h"
 
-pm_mainframe::pm_mainframe(wxWindow *parent) : mainframe(parent){
+pm_mainframe::pm_mainframe(wxWindow *parent) : mainframe(parent) {
 
-    list_project();
-
+  list_project();
 }
-pm_aboutframe::pm_aboutframe(wxWindow *parent) : aboutframe(parent){}
+pm_aboutframe::pm_aboutframe(wxWindow *parent) : aboutframe(parent) {}
 
 // function that add projects to listbox
-void pm_mainframe::list_project(){
+void pm_mainframe::list_project() {
 
-    vector<project> *a = read_project();
+  vector<project> *a = read_project();
 
-    if(a == nullptr) return;
+  if (a == nullptr)
+    return;
 
-    this->project_listBox->Clear();
+  this->project_listBox->Clear();
 
-    for(auto i : *a){
+  for (auto i : *a) {
 
-        this->project_listBox->Append(i.name);
-
-    }
-
+    this->project_listBox->Append(i.name);
+  }
 }
 
 // refresh listbox every time the window is activated
-void pm_mainframe::refresh_list(wxActivateEvent& event){
+void pm_mainframe::refresh_list(wxActivateEvent &event) { list_project(); }
 
-    list_project();
+void pm_mainframe::about_screen(wxCommandEvent &event) {
 
-}
-
-void pm_mainframe::about_screen(wxCommandEvent& event){
-
-    wxFrame *about_frame = new pm_aboutframe(NULL);
-    about_frame->Show(true);
+  wxFrame *about_frame = new pm_aboutframe(NULL);
+  about_frame->Show(true);
 }
